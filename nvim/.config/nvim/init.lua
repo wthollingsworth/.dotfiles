b5094fab90999feb51f2b6d910964268c116deb6
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2021-07-27 21:05:39 -0400

    use vim-surround, convenience method to dump a lua table

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,3 +7,9 @@ local modules = {
 for i = 1, #modules, 1 do
 	pcall(require, modules[i])
 end
+
+function _G.dump(...)
+    local objects = vim.tbl_map(vim.inspect, {...})
+    print(unpack(objects))
+    return ...
+end
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -27,6 +27,7 @@ return require('packer').startup({function(use)
     }
 
     use 'tpope/vim-commentary'
+    use 'tpope/vim-surround'
 
     use 'justinmk/vim-sneak'
 
