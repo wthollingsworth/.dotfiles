262d23ccdae71f91422296c2921897e97ae7d30d
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2021-12-11 09:10:20 -0500

    enable endwise behavior in nvim-autopairs

--- nvim/.config/nvim/lua/plugins/options/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/options/nvim-autopairs.lua
@@ -1,11 +1,14 @@
+local autopairs = require('nvim-autopairs')
 local cmp = require('cmp')
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done())
 
-require'nvim-autopairs'.setup {
+autopairs.setup {
   -- use treesitter to check for a pair
   check_ts = true,
 
   -- autoindent
   map_cr = true,
 }
+
+autopairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
