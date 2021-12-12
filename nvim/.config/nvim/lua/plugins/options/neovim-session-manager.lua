d67ceba0ba8a2523b02308cbd47aa76b13c2ab24
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2021-12-12 13:33:48 -0500

    Load telescope extension for sessions

--- nvim/.config/nvim/lua/plugins/options/neovim-session-manager.lua
+++ nvim/.config/nvim/lua/plugins/options/neovim-session-manager.lua
@@ -1,4 +1,6 @@
 local config = require('session_manager.config')
-require('session_manager').setup({
+require'session_manager'.setup {
   autoload_mode = config.AutoloadMode.Disabled,
-})
+}
+
+require'telescope'.load_extension('sessions')
