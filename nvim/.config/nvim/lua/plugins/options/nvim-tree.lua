c0e12630e2aa7025ea98d7e469eb8ad562847c04
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2022-01-11 18:44:00 -0500

    Uncomment which-key mapping registrations
    I had commented these out when debugging which-key problems.
    Need to uncomment to get human-readable info in wk popup.

--- nvim/.config/nvim/lua/plugins/options/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/options/nvim-tree.lua
@@ -1,22 +1,22 @@
 -- Show indent markers for open folders.  Can't be set in setup function
 -- yet, and must be set before calling setup.
 vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_root_folder_modifier = ':.:t'
+vim.g.nvim_tree_root_folder_modifier = ":.:t"
 vim.g.nvim_tree_add_trailing = 1
 
-require'nvim-tree'.setup {
-  auto_close = true, -- close nvim-tree if it is the last open window
-  update_cwd = true, -- update tree root when directory changes
-  diagnostics = {
-    enable = true,
-  },
-}
+require("nvim-tree").setup({
+	auto_close = true, -- close nvim-tree if it is the last open window
+	update_cwd = true, -- update tree root when directory changes
+	diagnostics = {
+		enable = true,
+	},
+})
 
 local keymap = vim.keymap
-keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
+keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
 
-local wk = require('which-key')
---wk.register({ ['<C-n>'] = { nil, 'Toggle NvimTree pane' } })
+local wk = require("which-key")
+wk.register({ ["<C-n>"] = { nil, "Toggle NvimTree pane" } })
 
 --vim.cmd [[highlight NvimTreeNormal guibg=#3B4252]]
 --vim.cmd [[highlight NvimTreeVertSplit guifg=#3B4252 guibg=#3B4252]]
@@ -35,4 +35,4 @@ local wk = require('which-key')
 --
 -- So instead we'll just change the color of the tildes, which has
 -- the same result visually.
-vim.cmd [[ highlight NvimTreeEndOfBuffer guifg=bg ]]
+vim.cmd([[ highlight NvimTreeEndOfBuffer guifg=bg ]])
--- nvim/.config/nvim/lua/plugins/options/telescope.lua
+++ nvim/.config/nvim/lua/plugins/options/telescope.lua
@@ -1,63 +1,47 @@
-local telescope = require('telescope')
+local telescope = require("telescope")
 
-telescope.setup {
-  extensions = {
-    ['ui-select'] = {
-      require('telescope.themes').get_dropdown { }
-    },
-  },
-}
+telescope.setup({
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({}),
+		},
+	},
+})
 
-telescope.load_extension('fzf')
-telescope.load_extension('ui-select')
+telescope.load_extension("fzf")
+telescope.load_extension("ui-select")
 
 -- Check the documentation for more pickers, or use tab completion on
 -- the arguments to :Telescope.  Or just run :Telescope to search for
 -- them in Telescope.
 local keymap = vim.keymap
-local mappings = { f = { name = 'telescope' } }
-local opts = { prefix = '<leader>' }
+local mappings = { f = { name = "telescope" } }
+local opts = { prefix = "<leader>" }
 
 -- Fuzzy file search
-keymap.set(
-  'n',
-  '<leader>ff',
-  function()
-    require('telescope.builtin').find_files()
-  end
-)
---mappings.f.f = { nil, 'Find File' }
+keymap.set("n", "<leader>ff", function()
+	require("telescope.builtin").find_files()
+end)
+mappings.f.f = { nil, "Find File" }
 
 -- Invoke Ripgrep from Neovim
-keymap.set(
-  'n',
-  '<leader>fg',
-  function()
-    require('telescope.builtin').live_grep()
-  end
-)
---mappings.f.g = { nil, 'Grep Files' }
+keymap.set("n", "<leader>fg", function()
+	require("telescope.builtin").live_grep()
+end)
+mappings.f.g = { nil, "Grep Files" }
 
 -- Like BufExplorer
-keymap.set(
-  'n',
-  '<leader>fb',
-  function()
-    require('telescope.builtin').buffers()
-  end
-)
---mappings.f.b = { nil, 'Find Buffer' }
+keymap.set("n", "<leader>fb", function()
+	require("telescope.builtin").buffers()
+end)
+mappings.f.b = { nil, "Find Buffer" }
 
 -- Search Neovim help pages.
-keymap.set(
-  'n',
-  '<leader>fh',
-  function()
-    require('telescope.builtin').help_tags()
-  end
-)
---mappings.f.h = { nil, 'Find Neovim Help' }
+keymap.set("n", "<leader>fh", function()
+	require("telescope.builtin").help_tags()
+end)
+mappings.f.h = { nil, "Find Neovim Help" }
 
 -- Tell which-key about the mappings.
-local wk = require('which-key')
---wk.register(mappings, opts)
+local wk = require("which-key")
+wk.register(mappings, opts)
