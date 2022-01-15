91988431124064c08c47f081faac2d2c0c3bbdd6
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2022-01-14 19:30:36 -0500

    Disable solargraph document formatting.
    Neovim's default formatting handler asks which server to use
    for formatting when multiple servers attached to the buffer
    have formatting capabilities.  It asks which one you want to
    use with vim.ui.select.  I have this piping into Telescope
    but for whatever reason it doesn't let me select anything.
    Since I want to use null-ls anyway, disabling solargraph
    removes the pain point.

--- nvim/.config/nvim/lua/plugins/options/nvim-lsp-installer.lua
+++ nvim/.config/nvim/lua/plugins/options/nvim-lsp-installer.lua
@@ -1,59 +1,65 @@
-local lsp_installer = require('nvim-lsp-installer')
+local lsp_installer = require("nvim-lsp-installer")
 
 -- servers and associated configuration
 local desired_servers = {
-  sumneko_lua = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          -- Get the language server to recognize the 'vim', 'use' global
-          globals = {'vim', 'use'},
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-          enable = false,
-        },
-      },
-    }
-  },
-  solargraph = {},
-  html = {},
-  cssls = {},
-  tsserver = {},
-  jsonls = {},
-  vuels = {},
-  emmet_ls = {},
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				diagnostics = {
+					-- Get the language server to recognize the 'vim', 'use' global
+					globals = { "vim", "use" },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	},
+	solargraph = {
+		-- let null-ls handle formatting
+		on_attach = function(client)
+			client.resolved_capabilities.document_formatting = false
+			client.resolved_capabilities.document_range_formatting = false
+		end,
+	},
+	html = {},
+	cssls = {},
+	tsserver = {},
+	jsonls = {},
+	vuels = {},
+	emmet_ls = {},
 }
 
 -- ensure servers are installed
 for server_name, _ in pairs(desired_servers) do
-  local ok, server = lsp_installer.get_server(server_name)
-  if ok and not server:is_installed() then
-      server:install()
-  end
+	local ok, server = lsp_installer.get_server(server_name)
+	if ok and not server:is_installed() then
+		server:install()
+	end
 end
 
 -- use the built-in lsp as a completion source form cmp
 lsp_installer.on_server_ready(function(server)
-  local opts = desired_servers[server.name]
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  local cmp_nvim_lsp = require('cmp_nvim_lsp')
-  capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+	local opts = desired_servers[server.name]
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
-  opts.capabilities = capabilities
-  server:setup(opts)
+	opts.capabilities = capabilities
+	server:setup(opts)
 end)
 
 lsp_installer.settings({
-  ui = {
-    icons = {
-      server_installed = "✓",
-      server_pending = "➜",
-      server_uninstalled = "✗"
-    }
-  }
+	ui = {
+		icons = {
+			server_installed = "✓",
+			server_pending = "➜",
+			server_uninstalled = "✗",
+		},
+	},
 })
