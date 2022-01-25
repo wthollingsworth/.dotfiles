b24b9867cf19aa575033b42b2eeacb3c83ab2ace
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2022-01-25 15:53:42 -0500

    Update toggleterm mapping
    The mapping is applied to multiple modes, which is not great
    for `gt` as I set it up.

--- nvim/.config/nvim/lua/mappings/init.lua
+++ nvim/.config/nvim/lua/mappings/init.lua
@@ -27,3 +27,5 @@ keymap.set("n", "<C-h>", "<C-w>h")
 keymap.set("n", "<C-j>", "<C-w>j")
 keymap.set("n", "<C-k>", "<C-w>k")
 keymap.set("n", "<C-l>", "<C-w>l")
+
+keymap.set("n", "gt", ":ToggleTerm<CR>")
--- nvim/.config/nvim/lua/plugins/options/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/options/toggleterm.lua
@@ -1,5 +1,7 @@
 require("toggleterm").setup({
-	open_mapping = "gt",
+	--This option sets the mapping in multiple modes, including insert,
+	--and I like gt better than the suggested <C-\>
+	-- open_mapping = "gt",
 	shade_terminals = false,
 	direction = "horizontal",
 	float_opts = {
