9a931fcffa28ca6265b38a628eafe9237e66c602
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2022-09-28 17:02:06 -0600

    nvim: add simple plugin spec utility

---
+++ nvim/.config/nvim/lua/plugins/util/specification.lua
@@ -0,0 +1,33 @@
+local Specification = {}
+
+Specification.for_plugin = function(repo)
+  local self = {}
+
+  -- Many plugins use the cute extention `.nvim`.
+  -- Note that because the `.` is not a word character,
+  -- this regex silently ignores that extension.
+  local basename = repo:match("%w+/(%w+)")
+
+  local spec = {
+    repo,
+    config = function()
+      local present, plugin = pcall(require, basename)
+      if present then
+        plugin.setup({})
+      end
+    end
+  }
+
+  function self.with(tbl)
+    spec = vim.tbl_deep_extend("force", spec, tbl)
+    return self
+  end
+
+  function self.spec()
+    return spec
+  end
+
+  return self
+end
+
+return Specification
