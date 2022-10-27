f06237542b5b92c3820c950f5a78abc664e08451
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2022-10-27 10:45:54 -0600

    nvim: cmp-lsp breaking change

--- nvim/.config/nvim/lua/lsp/defaults.lua
+++ nvim/.config/nvim/lua/lsp/defaults.lua
@@ -50,7 +50,6 @@ M.on_attach = function(client, bufnr)
 end
 
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
