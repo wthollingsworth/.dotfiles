69a3f7494fe0aa3330e6f107afc6c6c94fa9c363
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2022-12-25 15:09:45 -0700

    nvim: set some lazy opts

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,4 +3,22 @@ require("keybindings")
 
 local util = require("plugin.util")
 util.bootstrap_plugin_manager()
-require("lazy").setup("plugin.spec")
+require("lazy").setup("plugin.spec", {
+  install = {
+    colorscheme = { "catppuccin" }
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin"
+      }
+    }
+  }
+})
