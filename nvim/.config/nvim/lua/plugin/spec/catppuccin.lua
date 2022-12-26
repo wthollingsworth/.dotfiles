86f4482ae21f15d108269c2de3e86d3a4e3ae91d
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2022-12-25 20:59:39 -0700

    nvim: dim inactive windows

--- nvim/.config/nvim/lua/plugin/spec/catppuccin.lua
+++ nvim/.config/nvim/lua/plugin/spec/catppuccin.lua
@@ -6,6 +6,9 @@ return {
       catppuccin = require("catppuccin")
       catppuccin.setup({
         flavour = "frappe",
+        dim_inactive = {
+          enabled = true,
+        }
       })
       vim.cmd.colorscheme("catppuccin")
     end
