133e8bedc0343dea6dde19fce5edc5cf841f5f25
William T. Hollingsworth <william.t.hollingsworth@protonmail.com>
2022-12-26 16:31:07 -0700

    nvim: basic lsp configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 require("options")
 
 require("plugin.conf")
+require("lsp.conf")
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
---
+++ nvim/.config/nvim/lua/lsp/conf.lua
@@ -0,0 +1,12 @@
+require("lsp.keymaps")
+
+local lspconfig = require("lspconfig")
+lspconfig.sumneko_lua.setup({
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace"
+      }
+    }
+  }
+})
